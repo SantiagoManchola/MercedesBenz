d2ca7f03cfe605b8fb605d3c80c95c6069a3bf57
SantiagoManchola <dmancholas@hotmail.com>
2024-06-18 18:58:49 -0500

    homeSection for cars

--- wp-content/themes/Mercedes-Benz/index.php
+++ wp-content/themes/Mercedes-Benz/index.php
@@ -8,14 +8,200 @@
 <body>
     <main>
         <?php get_header(); ?>
-        <?php get_avancedSearch() ?>
         <div class="swiper">
             <div class="swiper-wrapper">
                 <div class="swiper-slide"><img src="<?php echo get_template_directory_uri(); ?>/assets/images/image(1).png" alt="" alt=""></div>
                 <div class="swiper-slide"><img src="<?php echo get_template_directory_uri(); ?>/assets/images/image(1).png" alt="" alt=""></div>
                 <div class="swiper-slide"><img src="<?php echo get_template_directory_uri(); ?>/assets/images/image(1).png" alt="" alt=""></div>
             </div>
-            <div class="swiper-scrollbar"></div>
+            <div id="swiper-scrollbar" class="swiper-scrollbar"></div>
+        </div>
+        <div class="mainMessage">
+            <div class="title">Excelencia en cada kilómetro</div>
+            <div class="message">Encontrá tu <span class="mercedesWord">Mercedes-Benz</span> usado con la garantía de calidad y lujo que merecés.</div>
+        </div>
+        <div class="homeSection">
+            <?php get_avancedSearch() ?>
+            <div class="rightSection">
+                <h2>Publicaciones <span>destacadas</span></h2>
+                <div class="cardsContainer">
+                    <div class="cardInfo">
+                        <img class="cardImage" src="<?php echo get_template_directory_uri(); ?>/assets/images/cardImage.png" alt="">
+                        <div class="name">52 E 300e AZUL</div>
+                        <div class="value">
+                            <img src="<?php echo get_template_directory_uri(); ?>/assets/icons/home/ticket.svg" alt="">
+                            <div class="price">
+                                <div class="badge">
+                                    $
+                                    <span class="country">USD</span>
+                                </div>
+                                <div class="amount">215,000</div>
+                            </div>
+                        </div>
+                        <div class="features">
+                            <div class="feature">
+                                <img src="<?php echo get_template_directory_uri(); ?>/assets/icons/home/winshield.svg" alt="">
+                                <span class="description">3,807 Km</span>
+                            </div>
+                            <div class="feature">
+                                <img src="<?php echo get_template_directory_uri(); ?>/assets/icons/home/card.svg" alt="">
+                                <span class="description">Gasolina</span>
+                            </div>
+                            <div class="feature">
+                                <img src="<?php echo get_template_directory_uri(); ?>/assets/icons/home/wheel.svg" alt="">
+                                <span class="description">2023</span>
+                            </div>
+                        </div>
+                        <a href="">Más información <img src="<?php echo get_template_directory_uri(); ?>/assets/icons/home/diagonalArrow.svg" alt=""></a>
+                    </div>
+                    <div class="cardInfo">
+                        <img class="cardImage" src="<?php echo get_template_directory_uri(); ?>/assets/images/cardImage.png" alt="">
+                        <div class="name">52 E 300e AZUL</div>
+                        <div class="value">
+                            <img src="<?php echo get_template_directory_uri(); ?>/assets/icons/home/ticket.svg" alt="">
+                            <div class="price">
+                                <div class="badge">
+                                    $
+                                    <span class="country">USD</span>
+                                </div>
+                                <div class="amount">215,000</div>
+                            </div>
+                        </div>
+                        <div class="features">
+                            <div class="feature">
+                                <img src="<?php echo get_template_directory_uri(); ?>/assets/icons/home/winshield.svg" alt="">
+                                <span class="description">3,807 Km</span>
+                            </div>
+                            <div class="feature">
+                                <img src="<?php echo get_template_directory_uri(); ?>/assets/icons/home/card.svg" alt="">
+                                <span class="description">Gasolina</span>
+                            </div>
+                            <div class="feature">
+                                <img src="<?php echo get_template_directory_uri(); ?>/assets/icons/home/wheel.svg" alt="">
+                                <span class="description">2023</span>
+                            </div>
+                        </div>
+                        <a href="">Más información <img src="<?php echo get_template_directory_uri(); ?>/assets/icons/home/diagonalArrow.svg" alt=""></a>
+                    </div>
+                    <div class="cardInfo">
+                        <img class="cardImage" src="<?php echo get_template_directory_uri(); ?>/assets/images/cardImage.png" alt="">
+                        <div class="name">52 E 300e AZUL</div>
+                        <div class="value">
+                            <img src="<?php echo get_template_directory_uri(); ?>/assets/icons/home/ticket.svg" alt="">
+                            <div class="price">
+                                <div class="badge">
+                                    $
+                                    <span class="country">USD</span>
+                                </div>
+                                <div class="amount">215,000</div>
+                            </div>
+                        </div>
+                        <div class="features">
+                            <div class="feature">
+                                <img src="<?php echo get_template_directory_uri(); ?>/assets/icons/home/winshield.svg" alt="">
+                                <span class="description">3,807 Km</span>
+                            </div>
+                            <div class="feature">
+                                <img src="<?php echo get_template_directory_uri(); ?>/assets/icons/home/card.svg" alt="">
+                                <span class="description">Gasolina</span>
+                            </div>
+                            <div class="feature">
+                                <img src="<?php echo get_template_directory_uri(); ?>/assets/icons/home/wheel.svg" alt="">
+                                <span class="description">2023</span>
+                            </div>
+                        </div>
+                        <a href="">Más información <img src="<?php echo get_template_directory_uri(); ?>/assets/icons/home/diagonalArrow.svg" alt=""></a>
+                    </div>
+                    <div class="cardInfo">
+                        <img class="cardImage" src="<?php echo get_template_directory_uri(); ?>/assets/images/cardImage.png" alt="">
+                        <div class="name">52 E 300e AZUL</div>
+                        <div class="value">
+                            <img src="<?php echo get_template_directory_uri(); ?>/assets/icons/home/ticket.svg" alt="">
+                            <div class="price">
+                                <div class="badge">
+                                    $
+                                    <span class="country">USD</span>
+                                </div>
+                                <div class="amount">215,000</div>
+                            </div>
+                        </div>
+                        <div class="features">
+                            <div class="feature">
+                                <img src="<?php echo get_template_directory_uri(); ?>/assets/icons/home/winshield.svg" alt="">
+                                <span class="description">3,807 Km</span>
+                            </div>
+                            <div class="feature">
+                                <img src="<?php echo get_template_directory_uri(); ?>/assets/icons/home/card.svg" alt="">
+                                <span class="description">Gasolina</span>
+                            </div>
+                            <div class="feature">
+                                <img src="<?php echo get_template_directory_uri(); ?>/assets/icons/home/wheel.svg" alt="">
+                                <span class="description">2023</span>
+                            </div>
+                        </div>
+                        <a href="">Más información <img src="<?php echo get_template_directory_uri(); ?>/assets/icons/home/diagonalArrow.svg" alt=""></a>
+                    </div>
+                    <div class="cardInfo">
+                        <img class="cardImage" src="<?php echo get_template_directory_uri(); ?>/assets/images/cardImage.png" alt="">
+                        <div class="name">52 E 300e AZUL</div>
+                        <div class="value">
+                            <img src="<?php echo get_template_directory_uri(); ?>/assets/icons/home/ticket.svg" alt="">
+                            <div class="price">
+                                <div class="badge">
+                                    $
+                                    <span class="country">USD</span>
+                                </div>
+                                <div class="amount">215,000</div>
+                            </div>
+                        </div>
+                        <div class="features">
+                            <div class="feature">
+                                <img src="<?php echo get_template_directory_uri(); ?>/assets/icons/home/winshield.svg" alt="">
+                                <span class="description">3,807 Km</span>
+                            </div>
+                            <div class="feature">
+                                <img src="<?php echo get_template_directory_uri(); ?>/assets/icons/home/card.svg" alt="">
+                                <span class="description">Gasolina</span>
+                            </div>
+                            <div class="feature">
+                                <img src="<?php echo get_template_directory_uri(); ?>/assets/icons/home/wheel.svg" alt="">
+                                <span class="description">2023</span>
+                            </div>
+                        </div>
+                        <a href="">Más información <img src="<?php echo get_template_directory_uri(); ?>/assets/icons/home/diagonalArrow.svg" alt=""></a>
+                    </div>
+                    <div class="cardInfo">
+                        <img class="cardImage" src="<?php echo get_template_directory_uri(); ?>/assets/images/cardImage.png" alt="">
+                        <div class="name">52 E 300e AZUL</div>
+                        <div class="value">
+                            <img src="<?php echo get_template_directory_uri(); ?>/assets/icons/home/ticket.svg" alt="">
+                            <div class="price">
+                                <div class="badge">
+                                    $
+                                    <span class="country">USD</span>
+                                </div>
+                                <div class="amount">215,000</div>
+                            </div>
+                        </div>
+                        <div class="features">
+                            <div class="feature">
+                                <img src="<?php echo get_template_directory_uri(); ?>/assets/icons/home/winshield.svg" alt="">
+                                <span class="description">3,807 Km</span>
+                            </div>
+                            <div class="feature">
+                                <img src="<?php echo get_template_directory_uri(); ?>/assets/icons/home/card.svg" alt="">
+                                <span class="description">Gasolina</span>
+                            </div>
+                            <div class="feature">
+                                <img src="<?php echo get_template_directory_uri(); ?>/assets/icons/home/wheel.svg" alt="">
+                                <span class="description">2023</span>
+                            </div>
+                        </div>
+                        <a href="">Más información <img src="<?php echo get_template_directory_uri(); ?>/assets/icons/home/diagonalArrow.svg" alt=""></a>
+                    </div>
+                </div>
+                <button><img src="<?php echo get_template_directory_uri(); ?>/assets/icons/home/darkCard.svg" alt="">Cargar más vehículos</button>
+            </div>
         </div>
     </main>
 </body>
